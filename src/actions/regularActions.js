dcce90278f47f41e1c3a36ea1d5f449848bbcdc3
wyao905 <warrenyca905@gmail.com>
2020-04-04 16:44:13 -0400

    added some seed data for instructors, students, and subjects

--- src/actions/regularActions.js
+++ src/actions/regularActions.js
@@ -1,10 +1,27 @@
-export const fetchCats = () => {
-    return (dispatch) => {
-        dispatch({type: 'LOADING_CATS'})
-        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
-            .then(response => {return response.json()})
-            .then(responseJSON => {
-                dispatch({ type: 'ADD_CATS', cats: responseJSON.images})
-            })
-    }
-}
+export const addAuthor = author => {
+    return {
+        type: 'ADD_AUTHOR',
+        author
+    };
+};
+
+export const addAuthor = author => {
+    return {
+        type: 'ADD_AUTHOR',
+        author
+    };
+};
+
+export const addAuthor = author => {
+    return {
+        type: 'ADD_AUTHOR',
+        author
+    };
+};
+
+export const addAuthor = author => {
+    return {
+        type: 'ADD_AUTHOR',
+        author
+    };
+};
--- virtual-classroom-api/db/seeds.rb
+++ virtual-classroom-api/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Instructors
+minerva = Instructor.create(name: "Minerva McGonagall")
+severus = Instructor.create(name: "Severus Snape")
+
+# Students
+ron = Student.create(name: "Ron Weasley")
+hermoine = Student.create(name: "Hermoine Granger")
+harry = Student.create(name: "Harry Potter")
+
+# Subjects
+transfiguration = Subject.create(name: "Transfiguration", instructor: minerva)
+potions = Subject.create(name: "Potions", instructor: severus)
+occlumency = Subject.create(name: "Occlumency", instructor: severus)
