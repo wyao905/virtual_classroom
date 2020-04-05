a5f2a69508930457e4f181722052096371237747
wyao905 <warrenyca905@gmail.com>
2020-04-05 13:40:51 -0400

    added custom sessions routes for create and destroy

--- virtual-classroom-api/config/routes.rb
+++ virtual-classroom-api/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :instructors
   resources :lectures
   resources :subjects
+  resources :session, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
