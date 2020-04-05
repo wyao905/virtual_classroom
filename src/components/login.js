e9e81af5f7d5a9371795d10e383b3875d3169920
wyao905 <warrenyca905@gmail.com>
2020-04-04 22:45:14 -0400

    built out login component and form

--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react'
+import Select from 'react-select'
 
 class Login extends Component {
     state = {
         email: "",
-        password: ""
+        password: "",
+        userSelectOption: null
+    }
+
+    handleChange = event => {
+        let newStateProp = {}
+        newStateProp[event.target.name] = event.target.value
+        this.setState(newStateProp)
+    }
+
+    handleSelectChange = event => {
+        this.setState({
+            userSelectOption: event.value
+        })
     }
 
     render() {
         return(
             <form>
-                <input type='text' value={this.state.email}/>
-                <input type='password' value={this.state.password}/>
-                <
+                <input name="email" type='text' value={this.state.email} onChange={event => this.handleChange(event)}/>
+                <input name="password" type='password' value={this.state.password} onChange={event => this.handleChange(event)}/>
+                <Select
+                    value={this.state.userSelectOption}
+                    onChange={event => this.handleSelectChange(event)}
+                    options={[
+                        {value: 'student', label: 'Student'},
+                        {value: 'instructor', label: 'Instructor'}
+                ]}/>
+                <input type="submit" value="Submit" />
             </form>
         )
     }
