db153049311a564ac34f8ab3f1960f54e7043cbd
wyao905 <warrenyca905@gmail.com>
2020-06-21 19:13:42 -0400

    removed instructor user check and code related to instructor from messagesContainer.js file under studentUser folder

--- src/components/messages/studentUser/messagesContainer.js
+++ src/components/messages/studentUser/messagesContainer.js
@@ -5,49 +5,49 @@ import MessageInput from './messageInput'
 class MessagesContainer extends Component {
     showMessage = () => {
         if(this.props.messagedTarget !== "") {
-            if(this.props.user.type === "student") {
-                let displayMessages = this.props.messages.filter(msg => msg.relationships.subject.data.id === this.props.messagedTarget)
-                if(displayMessages.length > 0) {
-                    let sorted = displayMessages.sort((a, b) => (a.created_at < b.created_at) ? 1 : -1)
-                    return sorted.map(msg => {
-                        let origin
-                        if(msg.attributes.sender === "student") {
-                            origin = "Sent to: "
-                        } else {
-                            origin = "Received from: "
-                        }
-                        return <ul><Message
-                            origin={origin}
-                            content={msg.attributes.content}
-                            target={this.props.instructors.find(i => i.id === msg.relationships.instructor.data.id)}
-                            date={msg.attributes.created_at.split("T")[0]}
-                            time={msg.attributes.created_at.split("T")[1]}/></ul>
-                    })
-                } else {
-                    return null
-                }
+            // if(this.props.user.type === "student") {
+            let displayMessages = this.props.messages.filter(msg => msg.relationships.subject.data.id === this.props.messagedTarget)
+            if(displayMessages.length > 0) {
+                let sorted = displayMessages.sort((a, b) => (a.created_at < b.created_at) ? 1 : -1)
+                return sorted.map(msg => {
+                    let origin
+                    if(msg.attributes.sender === "student") {
+                        origin = "Sent to: "
+                    } else {
+                        origin = "Received from: "
+                    }
+                    return <ul><Message
+                        origin={origin}
+                        content={msg.attributes.content}
+                        target={this.props.instructors.find(i => i.id === msg.relationships.instructor.data.id)}
+                        date={msg.attributes.created_at.split("T")[0]}
+                        time={msg.attributes.created_at.split("T")[1]}/></ul>
+                })
             } else {
-                let displayMessages = this.props.messages.filter(msg => msg.relationships.student.data.id === this.props.messagedTarget)
-                if(displayMessages.length > 0) {
-                    let sorted = displayMessages.sort((a, b) => (a.created_at < b.created_at) ? 1 : -1)
-                    return sorted.map(msg => {
-                        let origin
-                        if(msg.attributes.sender === "instructor") {
-                            origin = "Sent to: "
-                        } else {
-                            origin = "Received from: "
-                        }
-                        return <ul><Message
-                            origin={origin}
-                            content={msg.attributes.content}
-                            target={this.props.students.find(s => s.id === msg.relationships.student.data.id)}
-                            date={msg.attributes.created_at.split("T")[0]}
-                            time={msg.attributes.created_at.split("T")[1]}/></ul>
-                    })
-                } else {
-                    return null
-                }
+                return null
             }
+            // } else {
+            //     let displayMessages = this.props.messages.filter(msg => msg.relationships.student.data.id === this.props.messagedTarget)
+            //     if(displayMessages.length > 0) {
+            //         let sorted = displayMessages.sort((a, b) => (a.created_at < b.created_at) ? 1 : -1)
+            //         return sorted.map(msg => {
+            //             let origin
+            //             if(msg.attributes.sender === "instructor") {
+            //                 origin = "Sent to: "
+            //             } else {
+            //                 origin = "Received from: "
+            //             }
+            //             return <ul><Message
+            //                 origin={origin}
+            //                 content={msg.attributes.content}
+            //                 target={this.props.students.find(s => s.id === msg.relationships.student.data.id)}
+            //                 date={msg.attributes.created_at.split("T")[0]}
+            //                 time={msg.attributes.created_at.split("T")[1]}/></ul>
+            //         })
+            //     } else {
+            //         return null
+            //     }
+            // }
         } else {
             return null
         }
