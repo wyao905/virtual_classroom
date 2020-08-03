5cc23373c701355b40043f26b4a9d72204f7eb92
wyao905 <warrenyca905@gmail.com>
2020-08-02 23:34:00 -0400

    changed socket port to 3000 from 8000

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client'
 
-const socket = openSocket('http://localhost:8000')
+const socket = openSocket('http://localhost:3000')
 
 function updateLecture(lec) {
     socket.emit('lectureUpdated', lec)
