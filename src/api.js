b3fd33576f6086f07d7892b0f9085f5c8ff3fc45
wyao905 <warrenyca905@gmail.com>
2020-08-23 22:43:19 -0400

    test

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client'
 
-const socket = openSocket('http://localhost:3000')
+const socket = openSocket('https://vir-clsrm.herokuapp.com/')
 
 function updateLecture(lec) {
     socket.emit('lectureUpdated', lec)
