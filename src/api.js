1e66ec3b64c2349e810ec8dbd11e1107628d92b1
wyao905 <warrenyca905@gmail.com>
2020-08-23 22:35:05 -0400

    changed socket back to localhost

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client'
 
-const socket = openSocket('http://vir-clsrm.herokuapp.com/')
+const socket = openSocket('http://localhost:3000')
 
 function updateLecture(lec) {
     socket.emit('lectureUpdated', lec)
