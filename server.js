1f9279229347623730d8f143606f705f63f64b85
wyao905 <warrenyca905@gmail.com>
2020-09-20 13:00:07 -0400

    trying cors

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
+app.use(cors())
 app.use((req, res, next) => {
     // res.header("Access-Control-Allow-Origin", "*")
     // res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
